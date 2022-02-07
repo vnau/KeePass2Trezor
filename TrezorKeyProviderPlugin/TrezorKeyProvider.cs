4e9ee5662bd6dc42179c2bebcb5ca1ad045f0330
vnau <vnau@users.noreply.github.com>
2022-02-07 16:28:07 +0700

    project files moved

--- TrezorKeyProviderPlugin/TrezorKeyProvider.cs
+++ TrezorKeyProviderPlugin/TrezorKeyProvider.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TrezorKeyProviderPlugin.Forms;
 using TrezorKeyProviderPlugin.Device;
+using TrezorKeyProviderPlugin.Properties;
 
 namespace TrezorKeyProviderPlugin
 {
